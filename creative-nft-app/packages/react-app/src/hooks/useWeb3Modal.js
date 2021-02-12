79b0598a3bb8010ddeab2db68c1767b2faa12e48
Gawain B. II <bracy.gawain2014@gmail.com>
2021-02-11 22:40:32 -0500

    Update useWeb3Modal.js

--- creative-nft-app/packages/react-app/src/hooks/useWeb3Modal.js
+++ creative-nft-app/packages/react-app/src/hooks/useWeb3Modal.js
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useState } from "react";
 import { Web3Provider } from "@ethersproject/providers";
+// import Web3 from "web3";
 import Web3Modal from "web3modal";
 //import WalletConnectProvider from "@walletconnect/web3-provider";
 import Arkane from "@arkane-network/web3-arkane-provider";
@@ -15,32 +16,38 @@ function useWeb3Modal(config = {}) {
   const [autoLoaded, setAutoLoaded] = useState(false);
   const { autoLoad = true, infuraId = INFURA_ID, NETWORK = NETWORK_NAME } = config;
 
+  const providerOptions = {
+    /* See Provider Options Section */
+    arkane: {
+      package: Arkane, //required
+      options: {
+        clientId: 'Creative-Platform',
+        rpcUrl: `https://rinkeby.infura.io/v3/${infuraId}`, //optional
+        environment: 'staging', //optional, production by default
+        signMethod: 'POPUP' //optional, REDIRECT by default
+        // bearerTokenProvider: () => 'obtained_bearer_token', //optional, default undefined
+        // //optional: you can set an identity provider to be used when authenticating
+        // authenticationOptions: {
+        //   idpHint: 'google'
+        // }
+      }
+    }
+  }
   // Web3Modal also supports many other wallets.
   // You can see other options at https://github.com/Web3Modal/web3modal
   const web3Modal = new Web3Modal({
     network: NETWORK,
     cacheProvider: true,
-    providerOptions: {
-      walletconnect: {
-        package: Arkane,
-        options: {
-          clientId: 'Creative-Platform',
-          rpcUrl: 'https://kovan.infura.io/v3/'+ infuraId, //optional
-          environment: 'staging', //optional, production by default
-          signMethod: 'POPUP', //optional, REDIRECT by default
-          bearerTokenProvider: () => 'obtained_bearer_token', //optional, default undefined
-          //optional: you can set an identity provider to be used when authenticating
-          authenticationOptions: {
-            idpHint: 'google'
-          }
-        },
-      },
-    },
+    providerOptions
   });
 
-  Arkane.createArkaneProviderEngine(options).then(provider => {
-      web3 = new Web3(provider);
-  });
+  // const connectProvider = await web3Modal.connectTo(Arkane);
+  // const web = new Web3(provider);
+  
+
+  // Arkane.createArkaneProviderEngine(options).then(provider => {
+  //   web3 = new Web3(provider);
+  // });
 
   // Open wallet selection modal.
   const loadWeb3Modal = useCallback(async () => {
@@ -49,8 +56,8 @@ function useWeb3Modal(config = {}) {
   }, [web3Modal]);
 
   const logoutOfWeb3Modal = useCallback(
-    async function () {
-      await web3Modal.clearCachedProvider();
+    async () => {
+      web3Modal.clearCachedProvider();
       window.location.reload();
     },
     [web3Modal],
