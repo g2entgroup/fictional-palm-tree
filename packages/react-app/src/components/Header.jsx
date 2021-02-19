eefbc14c7e09385e2ce07020899d5a0806a302fb
G2 <sirgawain@g2entgroup.com>
2021-02-18 22:10:37 -0500

    content update
    Signed-off-by: G2 <sirgawain@g2entgroup.com>

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -7,8 +7,8 @@ export default function Header() {
   return (
     <a href="https://github.com/austintgriffith/scaffold-eth" target="_blank" rel="noopener noreferrer">
       <PageHeader
-        title="🏗 scaffold-eth"
-        subTitle="forkable Ethereum dev stack focused on fast product iteration"
+        title="📺 Creative Platform"
+        subTitle="The stage is YOURS..."
         style={{ cursor: "pointer" }}
       />
     </a>
