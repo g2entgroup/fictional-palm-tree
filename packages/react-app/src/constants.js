52c56ba6313a42ced254aec9b1e78942f4b5360f
G2 <sirgawain@g2entgroup.com>
2021-02-18 21:05:10 -0500

    Changed network
    Network is now  "Mumbai"
    
    Signed-off-by: G2 <sirgawain@g2entgroup.com>

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -1,8 +1,8 @@
 // MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
-export const INFURA_ID = "460f40a260564ac4a4f4b3fffb032dad";
+export const INFURA_ID = "ff5a5c50d1e649bdb777f80813747e3d";
 
 //MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
-export const ETHERSCAN_KEY = "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8";
+export const ETHERSCAN_KEY = "TW512IATPRQJE8VXT5SU4MDRZA2N7UT8WR";
 
 
 
