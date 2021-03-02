5a69fdd4ddb6c456b9d54cdfb9c92321cecc5b46
nicolasguasca1 <33971632+nicolasguasca1@users.noreply.github.com>
2021-03-02 05:55:01 +0100

    CP-12 (#29)
    * development environment setup
    
    * First successful compile for Creative Vendor upgradeable
    
    * ERC20 Pending
    
    * All trash files deleted
    
    Co-authored-by: G2 <sirgawain@g2entgroup.com>

---
+++ contracts/CreativeVendor.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier: GPL-2.0-only OR MIT
+
+pragma solidity >=0.6.0;
+
+import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+
+contract CreativeVendor is Initializable {
+
+    IERC20Upgradeable public token;
+
+    function initialize(IERC20Upgradeable _token) public initializer {
+        token = _token;
+    }
+    event Bought(uint256 amount);
+    event Sold(uint256 amount);
+
+    function buy() payable public {
+        uint256 amountTobuy = msg.value;
+        uint256 dexBalance = token.balanceOf(address(this));
+        require(amountTobuy > 0, "You need to send some Ether");
+        require(amountTobuy <= dexBalance, "Not enough tokens in the reserve");
+        token.transfer(msg.sender, amountTobuy);
+        emit Bought(amountTobuy);
+    }
+    
+    function sell(uint256 amount) public {
+        require(amount > 0, "You need to sell at least some tokens");
+        uint256 allowance = token.allowance(msg.sender, address(this));
+        require(allowance >= amount, "Check the token allowance");
+        token.transferFrom(msg.sender, address(this), amount);
+        msg.sender.transfer(amount);
+        emit Sold(amount);
+    }
+
+}
